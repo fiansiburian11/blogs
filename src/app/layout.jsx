6f97d32a9e2358bcb861f499f7dc69d829062d55
fiansiburian11 <bukanfirman11@gmail.com>
2025-02-10 11:58:36 +0700

    update

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,10 +1,30 @@
+"use client";
+
 import Footer from "@/components/ui/Footer";
 import "./globals.css";
+import Lenis from "lenis";
+import { useEffect } from "react";
 
 export default function RootLayout({ children }) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2, // Durasi animasi scroll
+      easing: (t) => 1 - Math.pow(1 - t, 3), // Fungsi easing
+      smooth: true, // Mengaktifkan smooth scrolling
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+
+    return () => lenis.destroy(); // Cleanup saat unmount
+  }, []);
+
   return (
     <html lang="en">
-      <body className="flex flex-col min-h-screen">
+      <body className="flex flex-col min-h-screen bg-gradient-to-b from-black to-cyan-900">
         <main className="flex-grow">{children}</main>
         <Footer />
       </body>
